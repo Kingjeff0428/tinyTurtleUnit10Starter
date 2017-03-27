ddc104f9e7004d5c280ee70b37b2acb9ae871bdc
Jeffrey Ng <jeffreyng0428@gmail.com>
2017-03-27 20:25:38 +0000

    finished making all the shapes

--- script.js
+++ script.js
@@ -11,8 +11,58 @@ forward(50);
 right(90);
 forward(50);
 }
-
+function triangle(){
+forward(50);
+right(120);
+forward(50);
+right(120);
+forward(50);
+}
+function shape(){
+triangle();
+forward(80);
+triangle();    
+}
+function hexagon(){
+forward(50);
+right(60);
+forward(50);
+right(60);
+forward(50);
+right(60);
+forward(50);
+right(60);
+forward(50);
+right(60);
+forward(50);
+}
+function rectangle(){
+forward(40);
+right(90);
+forward(100);
+right(90);
+forward(40);
+right(90);
+forward(100);
+}
+function house(){
+    square();
+    right(30);
+    triangle();
+}
+function pentagon(){
+forward(50);
+right(72);
+forward(50);
+right(72);
+forward(50);
+right(72);
+forward(50);
+right(72);
+forward(50);
+}
 // Type your function call below
-
-
-stamp();
+house();
+pentagon();
+hexagon();
+rectangle();
